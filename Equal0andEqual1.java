22b93a043006a5a120397e83e874f54a6df1169f
lokesh-rmd <ssue.it2024@rmd.ac.in>
2026-08-04 09:17:08 +0530

    Create Equal0andEqual1.java

---
+++ Equal0andEqual1.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class Equal0sand1s {
+
+    public static int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, -1);
+
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += (nums[i] == 0) ? -1 : 1;
+
+            if (map.containsKey(sum)) {
+                maxLen = Math.max(maxLen, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 1, 1, 1, 0};
+
+        System.out.println(findMaxLength(nums));
+    }
+}
